30eb7b09d9e28d031f12e309894f9c9123534696
Mpprabhu <prabhu92004@gmail.com>
2024-07-04 22:50:50 +0530

    CODE : Sticky Navigation

--- CODSOFT - Level 1 - Task 1/script.js
+++ CODSOFT - Level 1 - Task 1/script.js
@@ -1,3 +1,23 @@
-const skillsSection = document.querySelector(".skills-section");
-const inititalCoords = skillsSection.getBoundingClientRect();
-window.addEventListener;
+const header = document.querySelector(".header");
+const aboutSection = document.querySelector(".about-section");
+
+// Sticky Navigation
+const headerHeight = header.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    document.body.classList.add("sticky");
+  } else {
+    document.body.classList.remove("sticky");
+  }
+};
+
+const observer = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headerHeight}px`,
+});
+
+observer.observe(aboutSection);
