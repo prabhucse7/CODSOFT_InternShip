e2e078def0bba568c474f94046abc3913e6ac48f
Mpprabhu <prabhu92004@gmail.com>
2024-07-11 19:13:54 +0530

    CODE : Projects Section

--- CODSOFT - Level 1 - Task 1/script.js
+++ CODSOFT - Level 1 - Task 1/script.js
@@ -22,21 +22,3 @@ const observer = new IntersectionObserver(stickyNav, {
 });
 
 observer.observe(aboutSection);
-
-// Section Revealer
-const revealElement = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section-hidden");
-  observer.unobserve(entry.target);
-};
-
-const sectionObserver = new IntersectionObserver(revealElement, {
-  root: null,
-  threshold: 0.1,
-});
-
-allSections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add("section-hidden");
-});
